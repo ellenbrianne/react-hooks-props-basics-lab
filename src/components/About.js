5de8a744572e9f94ffdb60e2f39d3bfcbd8c5624
Ellen Bennett <ellenbennett@Ellens-MacBook-Air.local>
2023-06-06 19:22:51 -0600

    successfully passed down props to varying components

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,23 @@
 import React from "react";
+import Links from './Links';
 
-function About() {
+function About(props) {
+
+  const showBio = () => {
+    if (props.bio) {
+      return <p>{props.bio}</p>
+    } else {
+      return null
+    }
+  }
+
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {showBio()}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={props.github} linkedin={props.linkedin} />
     </div>
   );
 }
